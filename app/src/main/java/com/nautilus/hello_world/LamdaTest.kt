a1d9b6243d673ce53dd31c7f28b5d710e3376b97
Sonal Agrawal <sagrawal@nautilus.com>
2020-04-29 15:37:41 -0700

    Add sub function

--- app/src/main/java/com/nautilus/hello_world/LamdaTest.kt
+++ app/src/main/java/com/nautilus/hello_world/LamdaTest.kt
@@ -9,6 +9,9 @@ fun main(args: Array<String>) {
 
     val program = Program()
 
+    program.addTwoNumbers(2, 7)     // Simple way... for better understanding
+    program.subtraction(2, 7)     // Simple way... for better understanding
+
     program.addTwoNumbers(2, 7, object : MyInterface {   // Using Interface / OOPs way
 
         override fun execute(sum: Int) {
@@ -35,9 +38,19 @@ class Program {
         action.execute(sum)
     }
 
+    fun addTwoNumbers(a: Int, b: Int) {                         // Simple way.. Just for Better Understanding
+
+        val sum =  a + b
+        println(sum)
+    }
+
+    fun subtraction(a: Int, b: Int) {                         // Simple way.. Just for Better Understanding
+
+        val sum =  a + b
+        println(sum)
+    }
 }
 
 interface MyInterface {
     fun execute(sum: Int)
 }
-
